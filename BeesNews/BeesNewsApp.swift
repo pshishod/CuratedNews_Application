11c7dda2dc7f8f0e9ca7e87a7cf11198ba8c17cd
pshishod <33633060+pshishod@users.noreply.github.com>
2021-01-27 00:54:22 -0600

    Initial Commit

---
+++ BeesNews/BeesNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeesNewsApp.swift
+//  BeesNews
+//
+//  Created by Priyansh Shishodia on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct BeesNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
